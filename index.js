0ce5563d67ad4f655a3d02b1f9e21192d62612f0
jdfloresl <jdfloresl@utn.edu.ec>
2017-11-13 11:44:31 -0500

    inicio 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Universidad Tecnica Del Norte'+'Fecha: 13/11/2017'+'Integrantes: Tayan David, Flores David.')
+  response.send('Universidad Tecnica Del Norte'+'\n'+'Fecha: 13/11/2017'+'Integrantes: Tayan David, Flores David.')
 })
 
 app.listen(app.get('port'), function() {
