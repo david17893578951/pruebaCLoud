1e3bea7f7ac7ecb2d83bf1508a21c5a5f0945c17
jdfloresl <jdfloresl@utn.edu.ec>
2017-11-13 11:32:23 -0500

    nembrete

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Universidad Tecnica Del Norte')
+  response.send('Fecha: 13/11/2017')
+  response.send('Integrantes: Tayan David, Flores David')
 })
 
 app.listen(app.get('port'), function() {
